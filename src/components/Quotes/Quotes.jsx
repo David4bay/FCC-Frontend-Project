8828022e0cd0d2114b5eb293bdb87beee2e73064
David4bay <david4bay@gmail.com>
2023-05-23 07:54:21 +0100

    corrected twitter link

--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -8,7 +8,7 @@ function Quotes({data, handleClick}) {
             <div className="Quote__Box" id="quote-box">
                         <div className="Text" id="text">{data.text}</div>
                         <button className='btn' type='button' onClick={handleClick} id="new-quote">New Quote</button>
-                        <a href={`"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${data.text}"`} className="Tweet__Quote" id="tweet-quote" target='noreferrer noopener _blank'><FaTwitter /></a>
+                        <a href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${data.text}`} className="Tweet__Quote" id="tweet-quote" target='noreferrer _blank'><FaTwitter /></a>
                         <span className="Author" id="author">{data.author === " " ? 'Unknown' : data.author}</span>
                         <a href='https://github.com/David4bay' className="Creator">Coded by David Bayode</a>
             </div>
