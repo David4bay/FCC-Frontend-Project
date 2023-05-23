68d456e7887196f3e62982e41e47178d3f6cae3f
David4bay <david4bay@gmail.com>
2023-05-23 09:08:22 +0100

    fixed null author to return unknown

--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -2,14 +2,14 @@
 import { FaTwitter } from 'react-icons/fa'
 
 function Quotes({data, handleClick}) {
-
+    console.log(data);
     return (
         <>
             <div className="Quote__Box" id="quote-box">
                         <div className="Text" id="text">{data.text}</div>
                         <button className='btn' type='button' onClick={handleClick} id="new-quote">New Quote</button>
                         <a href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${data.text}`} className="Tweet__Quote" id="tweet-quote" target='noreferrer _blank'><FaTwitter /></a>
-                        <span className="Author" id="author">{data.author === " " ? 'Unknown' : data.author}</span>
+                        <span className="Author" id="author">{data.author === null ? 'Unknown' : data.author}</span>
                         <a href='https://github.com/David4bay' className="Creator">Coded by David Bayode</a>
             </div>
         </>
